78a0a4244ed8f643fcb27d5fbe2e60acef9f648a
R-lth <bluearth0502@gmail.com>
2026-07-02 16:19:49 +0900

    SerializeField. private 유지 가능하되, unity editor 상에서는 보이게 할 수 있음

--- hello/Assets/Script/Car.cs
+++ hello/Assets/Script/Car.cs
@@ -3,13 +3,13 @@ using UnityEngine.SocialPlatforms;
 
 public class Car : MonoBehaviour
 {
+    [SerializeField]
     float speed = 2f;
     
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+                                                                             
     }
 
     // Update is called once per frame
