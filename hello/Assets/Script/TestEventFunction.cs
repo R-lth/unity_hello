e9f9cb7bd75b7185066e98888fe0ccccd6437f36
R-lth <bluearth0502@gmail.com>
2026-07-02 16:12:23 +0900

    유니티 event 프레임워크 테스트

---
+++ hello/Assets/Script/TestEventFunction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TestEventFunction : MonoBehaviour
+{
+    void Awake() { print("Awake: " + Time.deltaTime + ", " + gameObject.name); }
+    void Start() { print("Start: " + Time.deltaTime + ", " + gameObject.name); }
+    void FixedUpdate() { print("FixedUpdate: " + Time.deltaTime + ", " + gameObject.name); }
+    void Update() { print("Update: " + Time.deltaTime + ", " + gameObject.name); }
+    void LateUpdate() { print("LateUpdate: " + Time.deltaTime + ", " + gameObject.name); }
+    void OnEnable() { print("OnEnable: " + Time.deltaTime + ", " + gameObject.name); }
+    void OnDisable() { print("OnDisable: " + Time.deltaTime + ", " + gameObject.name); }
+
+}
