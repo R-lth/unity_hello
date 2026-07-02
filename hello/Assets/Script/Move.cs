89d03d6c4eca10ab4b882878a068a32d3c94953d
R-lth <bluearth0502@gmail.com>
2026-07-02 14:31:14 +0900

    에셋 로드

---
+++ hello/Assets/Script/Move.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    float speed = 2f;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //this.transform.position += deltaTime * speed * Vector3.forward;
+
+        
+    }
+}
