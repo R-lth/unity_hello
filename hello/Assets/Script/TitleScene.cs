96ed1b260e5d6161d1485818339541591a36b424
R-lth <bluearth0502@gmail.com>
2026-07-09 14:46:20 +0900

    게임 시작 버튼 추가

---
+++ hello/Assets/Script/TitleScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScene : MonoBehaviour
+{
+    [SerializeField]
+    Button button;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        button.onClick.AddListener(SceneChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SceneChange() 
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+}
