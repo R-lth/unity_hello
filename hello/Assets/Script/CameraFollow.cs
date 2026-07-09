9c4b65cb7b4b0c4c70ef4e671fa2ed637b9764f5
R-lth <bluearth0502@gmail.com>
2026-07-09 14:01:32 +0900

    :white_check_mark: 차량 회전

---
+++ hello/Assets/Script/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) { return; }
+
+        transform.position = target.position + offset;
+    }
+}
--- hello/Assets/Script/Car.cs
+++ hello/Assets/Script/Car.cs
@@ -5,40 +5,48 @@ using UnityEngine.InputSystem;
 public class Car : MonoBehaviour
 {
     [SerializeField]
-    float speed = 2f;
+    float moveSpeed = 2f;
+    [SerializeField]
+    float rotateSpeed = 2f;
 
     Rigidbody rb;
-    Vector3 dir;
+    float turn = 0f;
+    Vector3 keyboardMove;
+    Vector2 mouseDelta;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
         gameObject.transform.position = Vector3.zero;
         gameObject.transform.rotation = Quaternion.identity;
-        //gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        turn = transform.eulerAngles.y;
     }
 
     private void OnMove(InputValue value)
     {
         Vector2 inputVector = value.Get<Vector2>();
-
-        // 2. 받아온 2D 입력값을 3D 공간의 방향(X축과 Z축)으로 재조립합니다.
-        dir = new Vector3(inputVector.x, 0f, inputVector.y);
+        keyboardMove = new Vector3(inputVector.x, 0f, inputVector.y);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnLook(InputValue value)
     {
-        
+        mouseDelta = value.Get<Vector2>();
     }
 
     private void FixedUpdate()
     {
-        if (dir.sqrMagnitude > 0) { dir.Normalize(); } // 대각선 움직임 처리
+        // 키보드 WASD
+        if (keyboardMove.sqrMagnitude > 0) { keyboardMove.Normalize(); } // 대각선 움직임 처리
 
-        Vector3 move = dir * Time.deltaTime * speed;
-        rb.MovePosition(rb.position + move);
+        Vector3 move = keyboardMove * Time.fixedDeltaTime * moveSpeed;
+        Vector3 worldMove = rb.rotation * move; // 키보드 입력은 오브젝트의 회전 값이 반영되지 않은 절대적인 월드 기준 방향이기 때문에, 플레이어의 회전 방향에 맞게 월드 방향을 바라보게 해야 함
+        rb.MovePosition(rb.position + worldMove); // rb.MovePosition()는 월드 좌표를 입력 받는 함수
+
+        // 마우스 회전
+        turn += mouseDelta.x * Time.fixedDeltaTime * rotateSpeed;
+        Quaternion rotate = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rotate);
     }
 }
